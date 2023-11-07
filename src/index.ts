6ad2f2518f40fac14a9bf7038bf2a8fed370d68a
bflatmajor <boring@boring.wang>
2023-11-07 15:58:49 +0800

    Remove redirection

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,4 @@ setTimeout(async () => {
     await new Typer(document.getElementById('words')).type(
         "Here's nothing, silly you! 😮"
     )
-    setTimeout(() => {
-        location.assign('https://live.bilibili.com/22363472')
-    }, 2000)
 }, Math.random() * 2000 + 3000)
